adda7a873c6639b700c29ad9884392b7d3aa5c3a
LUIZ FERNANDO COSTA <brclu232@BR.WCORP.CARREFOUR.COM>
2023-07-18 12:54:40 -0300

    Explorando as funcionalidades dos métodos implementadas na classe

--- vetores_bidimensionais.py
+++ vetores_bidimensionais.py
@@ -9,7 +9,7 @@ class Vector:
         self.y = y
 
     def __repr__(self):
-        return 'Vector(%r, %r)' % (self.x, self,y)
+        return 'Vector(%r, %r)' % (self.x, self.y)
 
     def __abs__(self):
         return hypot(self.x, self.y)
@@ -27,3 +27,27 @@ class Vector:
 
 # Apesar de ter implementado quarto dunder methods (além do __init__), nenhum deles é chamado diretamente
 # na classe ou no uso típico da classe ilustrado pelas listagens do console
+
+#O código a seguir testa o operador + para fazer a soma de vetores
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+v_soma = v1 + v2
+print(v_soma)
+
+#Calculando a magnitude de um vetor utilizado a função embutida abs
+
+v = Vector(3, 4)
+v_abs = abs(v)
+print(v_abs)
+
+#Utilizando o operaodr * para realizar a multiplicação por escalar dos vetores
+
+
+Multiplicaco_por_escalar = v * 3
+print(Multiplicaco_por_escalar)
+
+#e uma magnitude multiplicada
+
+magnitude_multiplicada = abs(v * 3)
+print(magnitude_multiplicada)
